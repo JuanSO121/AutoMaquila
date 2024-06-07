da0f716536fe325e6b6eb0193da0af6837a4d75c
JuanSO121 <sanchezjuanjose0508@gmail.com>
2024-06-07 09:42:05 -0500

    "Updated image upload paths and field names in Calzado and Categoria models, and updated template to reflect field name change."

--- apps/calzado/models.py
+++ apps/calzado/models.py
@@ -5,7 +5,7 @@ class Calzado(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    rutaImagen = models.ImageField(upload_to = '../media/calzado_imagenes/', blank=True, null=True, default='../media/calzado_imagenes/zapato.png')###############
+    rutaImagen = models.ImageField(upload_to = 'media/', blank=True, null=True, default='media/calzado_imagenes/zapato.png')###############
     
     def __str__(self):
         return f"{self.nombre} ({self.precio})"
@@ -16,7 +16,7 @@ class Calzado(models.Model):
 class Categoria(models.Model):
     pkCategoria = models.AutoField(primary_key=True)
     nombreCategoria = models.CharField(max_length=256, unique=True)
-    rutaImagen = models.ImageField(upload_to = '../uploads', default='../uploads/zapato.png')
+    rutaImagen = models.ImageField(upload_to = 'media/', default='media/zapato.png')
     
     def __str__(self):
         return f"{self.nombreCategoria}"
